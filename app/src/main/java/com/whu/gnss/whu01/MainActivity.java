6340b6e54ef15ad34066a9bbee39dad679937fa9
caizhihao <ccaius502@gmail.com>
2024-04-10 15:24:29 +0800

    修改地图中心点

--- app/src/main/java/com/whu/gnss/whu01/MainActivity.java
+++ app/src/main/java/com/whu/gnss/whu01/MainActivity.java
@@ -5,9 +5,10 @@ import android.os.Bundle;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapView;
-
+import org.osmdroid.wms.WMSTileSource;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,12 +19,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 配置 osmdroid
+//        //虚拟机调试
+//        mapView = findViewById(R.id.osmMapView);
+//        mapView.setMultiTouchControls(true);
+//        WMSTileSource wmsTileSource = new WMSTileSource( "OGC:WMS", new String[]{"http://202.114.122.22:2107/geoserver/whuXX/wms?service=WMS"},"whuXX:basemap","1.1.1","EPSG:900913","",256 );
+//        mapView.setTileSource(wmsTileSource);
+//        mapView.getController().setCenter(new GeoPoint(30.538,114.3618));
+//        mapView.setMinZoomLevel(15.0);
+//        mapView.setMaxZoomLevel(20.0);
+//        mapView.getController().setZoom(15.0);
+
+
+        // 华为手机调试
         Configuration.getInstance().setUserAgentValue(getPackageName());
         mapView = findViewById(R.id.osmMapView);
         mapView.setTileSource(TileSourceFactory.MAPNIK);
-        mapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT);
         mapView.setMultiTouchControls(true);
+        mapView.getController().setCenter(new GeoPoint(30.538, 114.3618));
+        mapView.getController().setZoom(15.0);
 
     }
     @Override
