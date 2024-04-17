eddadfd2ae2247ac859efb517e2ea1d8097cf649
caizhihao <ccaius502@gmail.com>
2024-04-17 16:33:29 +0800

    1

--- app/src/main/java/com/whu/gnss/whu01/MainActivity.java
+++ app/src/main/java/com/whu/gnss/whu01/MainActivity.java
@@ -3,9 +3,9 @@ package com.whu.gnss.whu01;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -21,20 +21,20 @@ import androidx.core.app.ActivityCompat;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
-import org.osmdroid.views.overlay.mylocation.SimpleLocationOverlay;
 import org.osmdroid.wms.WMSTileSource;
 
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private MapView mapView = null;
     private Button m_btnRoute;     //路径按钮
     private LocationManager locationManager;           //位置传感器管理器
+    private Marker marker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,14 +44,14 @@ public class MainActivity extends AppCompatActivity {
         //虚拟机调试
         mapView = findViewById(R.id.osmMapView);
         mapView.setMultiTouchControls(true);
-        WMSTileSource wmsTileSource = new WMSTileSource("OGC:WMS", new String[]{"http://202.114.122.22:2107/geoserver/whuXX/wms?service=WMS"}, "whuXX:basemap", "1.1.1", "EPSG:900913", "", 256);
+        WMSTileSource wmsTileSource = new WMSTileSource("OGC:WMS", new String[]{"http://202.114.122.22:2107/geoserver/whuB05/wms?service=WMS"}, "whuB05:basemap", "1.1.1", "EPSG:900913", "", 256);
         mapView.setTileSource(wmsTileSource);
         mapView.getController().setCenter(new GeoPoint(30.538, 114.3618));
         mapView.setMinZoomLevel(15.0);
         mapView.setMaxZoomLevel(20.0);
         mapView.getController().setZoom(15.0);
 
-//        // 华为手机调试
+        // 华为手机调试
 //        Configuration.getInstance().setUserAgentValue(getPackageName());
 //        mapView = findViewById(R.id.osmMapView);
 //        mapView.setTileSource(TileSourceFactory.MAPNIK);
@@ -81,16 +81,32 @@ public class MainActivity extends AppCompatActivity {
                 Manifest.permission.ACCESS_COARSE_LOCATION
         });
 
-        SimpleLocationOverlay overlay = new SimpleLocationOverlay(((BitmapDrawable) mapView.getContext().getDrawable(R.drawable.location)).getBitmap());
-        overlay.setLocation(new GeoPoint(30.538, 114.3618));
-        mapView.getOverlays().add(overlay);
+        marker = new Marker(mapView);
+        marker.setPosition(new GeoPoint(30.538, 114.3618)); // 设置标记点的位置
+        mapView.getOverlays().add(marker); // 将标记点添加到地图的覆盖层中
         LocationListener locationListener = new LocationListener() {
             // 当坐标改变时触发此函数，如果Provider传进相同的坐标，它就不会被触发
             @Override
             public void onLocationChanged(Location location) {
                 if (location != null) {
-                    overlay.setLocation(new GeoPoint(location.getLatitude(), location.getLongitude()));
+                    GeoPoint geoPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
+                    marker.setPosition(geoPoint); // 更新标记点的位置
                 }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                // Handle changes in location provider status
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                // Handle provider enablement
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                // Handle provider disablement
             }
         };
 
@@ -116,36 +132,9 @@ public class MainActivity extends AppCompatActivity {
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 1, locationListener);
         locationManager.addNmeaListener(mneaListener);
 
-        final String strrouteTask = "http://202.114.122.22:2107/geoserver/whuXX";
         m_btnRoute = findViewById(R.id.m_btnRoute);
-        m_btnRoute.setOnClickListener(v -> {
-            WhuRouteTask whuRouteTask = new WhuRouteTask("http://202.114.122.22:2107/geoserver/whuXX", "whuXX:route", new WhuRouteListner() {
-                @Override
-                public void onRoute(WhuRoute route) {
-                    //添加查询到的路径叠置图层
-                    mapView.getOverlays().add(route.getRouteGeometry());
-                    List<GeoPoint> points = route.getRouteGeometry().getActualPoints();
-                    //路径起点标注
-                    Marker startMarker = new Marker(mapView);
-                    startMarker.setPosition(points.get(0));
-                    startMarker.setTextLabelFontSize(30);
-                    startMarker.setTextIcon("S");
-                    //路径终点标注
-                    mapView.getOverlays().add(startMarker);
-                    Marker endMarker = new Marker(mapView);
-                    endMarker.setPosition(points.get(points.size()-1));
-                    endMarker.setTextLabelFontSize(30);
-                    endMarker.setTextIcon("E");
-                    mapView.getOverlays().add(endMarker);
-                    //强制重绘地图
-                    mapView.invalidate();
-                }
-            });
-            //设置路径查询起止点坐标
-            whuRouteTask.setStops(new GeoPoint(30.53717,114.3488),new GeoPoint(30.538,114.3718));
-            //执行异步路径查询
-            whuRouteTask.solveRouteAsync();
-        });
+        m_btnRoute.setOnClickListener(this);
+
 
     }
 
@@ -159,6 +148,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         mapView.onPause();
+    }
+    @Override
+    public void onClick(View v) {
+        //路径查询任务，设置服务地址，图层名，查询结果监听事件响应
+        WhuRouteTask whuRouteTask = new WhuRouteTask("http://202.114.122.22:2107/geoserver/whuXX", "whuXX:route", new WhuRouteListner() {
+            @Override
+            public void onRoute(WhuRoute route) {
+                //添加查询到的路径叠置图层
+                mapView.getOverlays().add(route.getRouteGeometry());
+
+                List<GeoPoint>  points = route.getRouteGeometry().getActualPoints();
+                //路径起点标注
+                Marker startMarker = new Marker(mapView);
+                startMarker.setPosition(points.get(0));
+                startMarker.setTextLabelFontSize(30);
+                startMarker.setTextIcon("S");
+                //路径终点标注
+                mapView.getOverlays().add(startMarker);
+                Marker endMarker = new Marker(mapView);
+                endMarker.setPosition(points.get(points.size()-1));
+                endMarker.setTextLabelFontSize(30);
+                endMarker.setTextIcon("E");
+                mapView.getOverlays().add(endMarker);
+                //强制重绘地图
+                mapView.invalidate();
+            }
+        });
+        //设置路径查询起止点坐标
+        whuRouteTask.setStops(new GeoPoint(30.53717,114.3488),new GeoPoint(30.538,114.3718));
+        //执行异步路径查询
+        whuRouteTask.solveRouteAsync();
     }
 
 }
